f45a39ad9ddb2a21dd3a79d648ec972fc2c15bac
Anatoly Scherbakov <altaisoft@gmail.com>
2023-09-03 15:46:12 +0300

    `cir-mapping-key-1-negative` is green again

--- yaml_ld/loader.py
+++ yaml_ld/loader.py
@@ -2,6 +2,4 @@ import yaml
 
 
 class YAMLLDLoader(yaml.SafeLoader):
-    # I do not understand how, but the below makes the system to parse a YAML
-    # document regardless of tags it contains, as per YAML-LD spec.
-    yaml_constructors = {}
+    pass
