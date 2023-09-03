07c49bc74947146ccd285d0a4a110db425edff42
Anatoly Scherbakov <altaisoft@gmail.com>
2023-09-03 11:12:26 +0300

    All working tests are green

--- yaml_ld/parse.py
+++ yaml_ld/parse.py
@@ -1,14 +1,18 @@
 import yaml
+from yaml.scanner import ScannerError
 
 from yaml_ld.errors import LoadingDocumentFailed
 from yaml_ld.models import Document
 
 
 def parse(yaml_string: str) -> Document:
-    document = yaml.load(
-        stream=yaml_string,
-        Loader=yaml.SafeLoader,
-    )
+    try:
+        document = yaml.load(
+            stream=yaml_string,
+            Loader=yaml.SafeLoader,
+        )
+    except ScannerError as err:
+        raise LoadingDocumentFailed(document='') from err
 
     if not isinstance(document, dict | list):
         raise LoadingDocumentFailed(document=document)
