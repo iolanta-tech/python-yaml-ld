126637f4839c6c88e381fed94584dc944418cd33
Anatoly Scherbakov <altaisoft@gmail.com>
2023-09-03 21:01:44 +0300

    Internationalization tags

--- yaml_ld/loader.py
+++ yaml_ld/loader.py
@@ -7,6 +7,10 @@ def xsd(term: str) -> str:
     return f'http://www.w3.org/2001/XMLSchema#{term}'
 
 
+def i18n(term: str) -> str:
+    return f'https://www.w3.org/ns/i18n#{term}'
+
+
 def tag(term: str) -> str:
     return f'tag:yaml.org,2002:{term}'
 
@@ -24,3 +28,7 @@ YAMLLDLoader.add_constructor(xsd('boolean'), YAMLLDLoader.construct_yaml_bool)
 YAMLLDLoader.add_constructor(xsd('date'), YAMLLDLoader.construct_scalar)
 YAMLLDLoader.add_constructor(xsd('time'), YAMLLDLoader.construct_scalar)
 YAMLLDLoader.add_constructor(xsd('dateTime'), YAMLLDLoader.construct_scalar)
+
+YAMLLDLoader.add_constructor(i18n('en-US'), YAMLLDLoader.construct_yaml_str)
+YAMLLDLoader.add_constructor(i18n('en-US_ltr'), YAMLLDLoader.construct_yaml_str)
+YAMLLDLoader.add_constructor(i18n('_rtl'), YAMLLDLoader.construct_yaml_str)
