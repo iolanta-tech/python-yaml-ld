390a94995aa12f258a8dfe6806bdb23d24feca45
Anatoly Scherbakov <altaisoft@gmail.com>
2023-10-29 12:43:23 +0400

    Incorrect mapping key errors handled

--- yaml_ld/parse.py
+++ yaml_ld/parse.py
@@ -1,11 +1,12 @@
 import yaml
 from yaml.composer import ComposerError
+from yaml.constructor import ConstructorError
 from yaml.scanner import ScannerError
 
 from yaml_ld.errors import (
     DocumentIsScalar,
     LoadingDocumentFailed,
-    UndefinedAliasFound,
+    UndefinedAliasFound, MappingKeyError,
 )
 from yaml_ld.loader import YAMLLDLoader
 from yaml_ld.models import Document
@@ -23,6 +24,12 @@ def parse(yaml_string: str) -> Document:
 
     except ComposerError as err:
         raise UndefinedAliasFound() from err
+
+    except ConstructorError as err:
+        if err.problem == 'found unhashable key':
+            raise MappingKeyError() from err
+
+        raise
 
     if not isinstance(document, (dict, list)):
         raise DocumentIsScalar(document=document)
