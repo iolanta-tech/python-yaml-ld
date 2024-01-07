d620de7bf8abbe735233ca15a0999088cf7ff45c
Anatoly Scherbakov <altaisoft@gmail.com>
2024-01-07 21:07:56 +0400

    Illustrate the issue with context from a local file

--- tests/test_expand.py
+++ tests/test_expand.py
@@ -1,4 +1,8 @@
+import pytest
+from pyld.jsonld import JsonLdError
+
 import yaml_ld
+from tests.common import specifications_root
 
 
 def test_empty_value():
@@ -13,3 +17,12 @@ def test_empty_value():
     }
 
     assert not yaml_ld.expand(document)
+
+
+@pytest.mark.xfail(
+    raises=JsonLdError,
+    reason='`pyld` does not handle `file://` paths.',
+)
+def test_local_context():
+    document = specifications_root / 'json-ld-api/tests/expand/0127-in.jsonld'
+    yaml_ld.expand(document)
