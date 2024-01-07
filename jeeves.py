7daeb0260e47e21737d53c800f15a8429d6ba380
Anatoly Scherbakov <altaisoft@gmail.com>
2024-01-08 01:00:46 +0400

    ➕ `j ci`

--- jeeves.py
+++ jeeves.py
@@ -1,6 +1,21 @@
-from sh import git
+from sh import git, pytest, tee
 
 
 def update_submodule():
     """Update the `specification` submodule from GitHub."""
     git.submodule.update('--remote', '--init', '--recursive')
+
+
+def ci():
+    """Run CI."""
+    tee(
+        'tests/coverage/pytest-coverage.txt',
+        _in=pytest(
+            'tests',
+            junitxml='tests/coverage/pytest.xml',
+            cov_report=['', 'html:tests/coverage/html'],
+            cov='yaml_ld',
+            _piped=True,
+            _ok_code={0, 1},
+        ),
+    )
