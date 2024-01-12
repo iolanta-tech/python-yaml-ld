ee09465571488d6f047f9b1255b5f41bbeab16a3
Anatoly Scherbakov <altaisoft@gmail.com>
2024-01-13 02:24:35 +0400

    `test_invalid_yaml` now 🟢

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,9 +1,13 @@
+import pytest
+
 import yaml_ld
 from tests.common import specifications_root
+from yaml_ld.errors import LoadingDocumentFailed
 
 
 def test_invalid_yaml():
     document = specifications_root / (
         'yaml-ld/tests/cases/cr-well-formed-2-negative-in.yamlld'
     )
-    assert yaml_ld.parse(document)
+    with pytest.raises(LoadingDocumentFailed):
+        yaml_ld.parse(document)
