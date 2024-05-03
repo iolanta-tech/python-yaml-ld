d7354a73e046669d202f01d9762a0172377d087d
Anatoly Scherbakov <altaisoft@gmail.com>
2024-05-03 22:47:59 +0400

    Interface of `compact()`

--- yaml_ld/compact.py
+++ yaml_ld/compact.py
@@ -1,12 +1,14 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import Field, validate_call
 from pyld import jsonld
 
 from yaml_ld.annotations import API
 from yaml_ld.errors import MappingKeyError, CycleDetected
+from yaml_ld.expand import except_json_ld_errors
 from yaml_ld.models import (
     Document, ProcessingMode, DocumentLoader, BaseOptions, ExtractAllScripts,
+    SerializedDocument,
 )
 from yaml_ld.parse import parse  # noqa: WPS347
 
@@ -20,55 +22,19 @@ class CompactOptions(BaseOptions):
     skip_expansion: bool = Field(default=False, alias='skipExpansion')
 
 
+@validate_call(config=dict(arbitrary_types_allowed=True))
 def compact(  # noqa: WPS211
-    document: str | bytes | Document,
-    context: Annotated[Document | None, 'Context to compact with.'],
-    base: Annotated[str | None, 'The base URL to use.'],
-    compact_arrays: Annotated[
-        bool,
-        'Compact arrays to single values when appropriate?',
-    ] = True,
-    graph: Annotated[
-        bool,
-        'Always output a top-level graph.',
-    ] = False,
-    extract_all_scripts: ExtractAllScripts = False,
-    mode: ProcessingMode = ProcessingMode.JSON_LD_1_1,
-    document_loader: DocumentLoader | None = None,
-    expand_context: Annotated[
-        Document | None,
-        'Context to expand the input with.',
-    ] = None,
-    skip_expansion: Annotated[
-        bool,
-        'Treat the input as already expanded, and do not expand it again.',
-    ] = False,
+    document: SerializedDocument | Document,
+    ctx: Annotated[Document | None, 'Context to compact with.'],
+    options: CompactOptions = CompactOptions(),
 ) -> Annotated[Document | list[Document], API / '#dom-jsonldprocessor-compact']:
     """Compact a JSON-LD document."""
     if isinstance(document, (str, bytes)):
         document = parse(document)
 
-    options = CompactOptions(
-        base=base,
-        extract_all_scripts=extract_all_scripts,
-        mode=mode,
-        document_loader=document_loader,
-        compact_arrays=compact_arrays,
-        graph=graph,
-        expand_context=expand_context,
-        skip_expansion=skip_expansion,
-    ).model_dump(
-        exclude_defaults=True,
-        by_alias=True,
-    )
-
-    try:
+    with except_json_ld_errors():
         return jsonld.compact(
             input_=document,
-            ctx=context,
-            options=options,
+            ctx=ctx,
+            options=options.model_dump(by_alias=True),
         )
-    except TypeError as err:
-        raise MappingKeyError() from err
-    except RecursionError as err:
-        raise CycleDetected() from err
