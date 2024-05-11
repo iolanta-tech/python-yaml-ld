1cab6bc1758d088782e47414015b13bfe1424078
Anatoly Scherbakov <altaisoft@gmail.com>
2024-05-12 00:13:46 +0400

    Document `ToRDFOptions`

--- yaml_ld/to_rdf.py
+++ yaml_ld/to_rdf.py
@@ -16,7 +16,16 @@ from yaml_ld.rdf import Dataset
 
 
 class ToRDFOptions(BaseOptions):
-    ...
+    """Options for converting YAML-LD to RDF."""
+
+    format: str = 'application/n-quads'
+    """The format to use to output a string: 'application/n-quads' for N-Quads."""
+
+    produce_generalized_rdf: bool = False
+    """True to output generalized RDF, false to produce only standard RDF."""
+
+    rdf_direction: str = 'i18n-datatype'
+    """Only 'i18n-datatype' supported."""
 
 
 @validate_call(config=dict(arbitrary_types_allowed=True))
