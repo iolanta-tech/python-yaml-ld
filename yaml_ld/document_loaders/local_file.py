e21a5a10bfc1a013fac2ed3a08958bb9c0377fea
Anatoly Scherbakov <altaisoft@gmail.com>
2024-06-13 23:56:55 +0400

    `html-manifest#te013` is now 🟢

--- yaml_ld/document_loaders/local_file.py
+++ yaml_ld/document_loaders/local_file.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any
 
 import yaml
+from pyld.jsonld import load_html
 from urlpath import URL
 
 from yaml_ld.document_loaders.base import DocumentLoader, PyLDResponse
@@ -9,10 +10,29 @@ from yaml_ld.document_loaders.base import DocumentLoader, PyLDResponse
 
 class LocalFileDocumentLoader(DocumentLoader):
     def __call__(self, source: str, options: dict[str, Any]) -> PyLDResponse:
-        with Path(URL(source).path).open() as f:
-            return {
-                'document': yaml.safe_load(f),
-                'documentUrl': source,
-                'contextUrl': None,
-                'contentType': 'application/ld+yaml',
-            }
+        path = Path(URL(source).path)
+
+        if path.suffix in {'.yaml', '.yml', '.yamlld'}:
+            with path.open() as f:
+                return {
+                    'document': yaml.safe_load(f),
+                    'documentUrl': source,
+                    'contextUrl': None,
+                    'contentType': 'application/ld+yaml',
+                }
+
+        if path.suffix in {'.html', '.xhtml'}:
+            with path.open() as f:
+                return {
+                    'document': load_html(
+                        input=f.read(),
+                        url=source,
+                        profile=None,
+                        options=options,
+                    ),
+                    'documentUrl': source,
+                    'contextUrl': None,
+                    'contentType': 'application/ld+yaml',
+                }
+
+        raise ValueError(f'Unknown file type: {source}')
