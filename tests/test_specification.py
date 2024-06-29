3e3ccf28fa9c6273015606f6284427626fc970a3
Anatoly Scherbakov <altaisoft@gmail.com>
2024-06-30 01:41:46 +0400

    `test_frame[frame-manifest#t0054]` is now 🟢

--- tests/test_specification.py
+++ tests/test_specification.py
@@ -385,14 +385,14 @@ def test_frame(
             parse=yaml_ld.parse,
             expand=yaml_ld.frame,
         )
-    except AssertionError:
+    except (AssertionError, FailureToFail):
         try:
             test_against_ld_library(
                 test_case=test_case,
                 parse=_load_json_ld,
                 expand=jsonld.frame,
             )
-        except AssertionError:
+        except (AssertionError, FailureToFail):
             pytest.skip('This test fails for pyld as well as for yaml-ld.')
         else:
             raise
