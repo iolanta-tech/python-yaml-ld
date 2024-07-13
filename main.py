a8c46ed8d36839c9bc29d8b35ac0bf09840eb259
Anatoly Scherbakov <altaisoft@gmail.com>
2024-07-14 01:44:32 +0400

    Make linters 🟢

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 """MkDocs macros for the documentation site."""
 import functools
-import os
-import shutil
-import tempfile
-import textwrap
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 import sh
 from mkdocs_macros.plugin import MacrosPlugin
-from sh import ErrorReturnCode, bash
-from sh import j as jeeves
-from sh import python
 
 STDERR_TEMPLATE = """
 !!! danger "Error"
@@ -39,21 +32,6 @@ JEEVES_TEMPLATE = """
 """
 
 
-TERMINAL_TEMPLATE = """
-``` title="↦ <code>{title}</code>"
-{output}
-```
-"""
-
-CODE_TEMPLATE = """
-```{language} title="{title}"
-{code}
-```
-
-{annotations}
-"""
-
-
 def format_annotations(annotations: List[str]) -> str:
     """Format annotations for mkdocs-material to accept them."""
     enumerated_annotations = enumerate(annotations, start=1)
@@ -64,39 +42,13 @@ def format_annotations(annotations: List[str]) -> str:
     )
 
 
-
-def code(
-    path: str,
-    docs_dir: Path,
-    language: Optional[str] = None,
-    title: Optional[str] = None,
-    annotations: Optional[List[str]] = None,
-    indent: Optional[int] = None,
-):
-    code_content = (docs_dir / path).read_text()
-
-    response = CODE_TEMPLATE.format(
-        language=language,
-        code=code_content,
-        title=title or path,
-        annotations=format_annotations(annotations or []),
-    )
-
-    if indent:
-        return textwrap.indent(
-            response,
-            ' ' * indent,
-        )
-
-    return response
-
-
-def run_python_script(
+def run_python_script(   # noqa: WPS210
     path: str,
     docs_dir: Path,
     annotations: Optional[List[str]] = None,
     args: Optional[List[str]] = None,
 ):
+    """Run a given Python file, embed results into docs."""
     if annotations is None:
         annotations = []
 
@@ -106,14 +58,14 @@ def run_python_script(
     code_path = docs_dir / path
     code = code_path.read_text()
 
+    stderr = None
     try:
-        stdout = python(
+        stdout = sh.python(
             *args,
             code_path,
             retcode=None,
             _cwd=code_path.parent,
         )
-        stderr = None
     except sh.ErrorReturnCode as error_return_code:
         stdout = error_return_code.stdout
         stderr = error_return_code.stdout
@@ -134,103 +86,15 @@ def run_python_script(
 
 
 def formatted_stderr(stderr: Optional[str]) -> str:
+    """Format the std err."""
     if not stderr:
         return ''
 
     return STDERR_TEMPLATE.format(stderr=stderr)
 
 
-def j(
-    path: Optional[str],
-    docs_dir: Path,
-    annotations: Optional[List[str]] = None,
-    args: Optional[List[str]] = None,
-    environment: Optional[Dict[str, str]] = None,
-):
-    environment = environment or {}
-
-    environment = {
-        **os.environ,
-        **environment,
-        'TERM': 'dumb',
-    }
-
-    if annotations is None:
-        annotations = []
-
-    if args is None:
-        args = []
-
-    with tempfile.TemporaryDirectory() as raw_directory:
-        directory = Path(raw_directory)
-
-        if path is not None:
-            code_path = docs_dir / path
-
-            if code_path.is_file():
-                (directory / 'jeeves.py').write_text(code_path.read_text())
-
-            else:
-                shutil.copytree(
-                    code_path,
-                    directory / 'jeeves',
-                )
-
-        try:
-            response = jeeves(
-                *args,
-                _cwd=directory,
-                _env=environment,
-                _tty_out=False,
-            )
-        except ErrorReturnCode as err:
-            stdout = err.stdout.decode() or '(stdout is empty)'
-            stderr = textwrap.indent(
-                err.stderr.decode(),
-                prefix='    ',
-            )
-        else:
-            stdout = response
-            stderr = None
-
-    cmd = 'j'
-    if args:
-        formatted_args = ' '.join(args)
-        cmd = f'{cmd} {formatted_args}'
-
-    return JEEVES_TEMPLATE.format(
-        path=path,
-        code=code,
-        stdout=stdout,
-        stderr=formatted_stderr(stderr),
-        annotations=format_annotations(annotations),
-        cmd=cmd,
-    )
-
-
-def terminal(
-    command: str,
-    title: Optional[str] = None,
-    environment: Optional[Dict[str, str]] = None,
-    cwd: Optional[str] = None,
-):
-    execute = bash['-c'].with_env(
-        **(environment or {}),
-    )
-
-    if cwd:
-        execute = execute.with_cwd(cwd)
-
-    output = execute(command)
-
-    return TERMINAL_TEMPLATE.format(
-        output=output,
-        title=title or command,
-    )
-
-
 def define_env(env: MacrosPlugin):
-    """Hook function."""
+    """Register macros."""
     env.macro(
         functools.partial(
             run_python_script,
@@ -238,5 +102,3 @@ def define_env(env: MacrosPlugin):
         ),
         name='run_python_script',
     )
-
-    env.macro(terminal)
