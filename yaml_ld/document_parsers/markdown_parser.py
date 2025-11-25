e34923d393b6dad827ae0412efdda9ffe6afe9d0
Anatoly Scherbakov <altaisoft@gmail.com>
2025-11-25 22:38:10 +0400

    #361 Update MarkdownDocumentParser to use ensure_not_scalar and fix line length

--- yaml_ld/document_parsers/markdown_parser.py
+++ yaml_ld/document_parsers/markdown_parser.py
@@ -6,7 +6,7 @@ from yaml_ld.document_parsers.base import (
     BaseDocumentParser,
     DocumentLoaderOptions,
 )
-from yaml_ld.document_parsers.yaml_parser import _ensure_not_scalar
+from yaml_ld.document_parsers.yaml_parser import ensure_not_scalar
 from yaml_ld.errors import InvalidEncoding, LoadingDocumentFailed
 from yaml_ld.models import JsonLdRecord
 
@@ -27,8 +27,10 @@ class MarkdownDocumentParser(BaseDocumentParser):
             raise InvalidEncoding() from unicode_decode_error
 
         try:
-            metadata, _content = frontmatter.parse(markdown_content)
+            metadata, _content = (  # noqa: WPS110
+                frontmatter.parse(markdown_content)
+            )
         except Exception as parse_error:
             raise LoadingDocumentFailed(path=source) from parse_error
 
-        return _ensure_not_scalar(metadata)
+        return ensure_not_scalar(metadata)
