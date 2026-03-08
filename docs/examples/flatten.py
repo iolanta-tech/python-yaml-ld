1d641ff987144767de17ad2a2fcaffea5da24bc6
Anatoly Scherbakov <altaisoft@gmail.com>
2026-03-08 19:43:34 +0400

    Use ruamel.yaml for YAML output

--- docs/examples/flatten.py
+++ docs/examples/flatten.py
@@ -1,5 +1,10 @@
-import yaml
+import io
+
+from ruamel.yaml import YAML
 
 import yaml_ld
 
-print(yaml.dump(yaml_ld.flatten('pythagorean-theorem.yamlld')))
+yaml = YAML(typ='safe')
+stream = io.StringIO()
+yaml.dump(yaml_ld.flatten('pythagorean-theorem.yamlld'), stream)
+print(stream.getvalue())  # noqa: WPS421
