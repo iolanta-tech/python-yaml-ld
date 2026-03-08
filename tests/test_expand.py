537d79ea91b9a8b551f4097df125348e42ebd446
Anatoly Scherbakov <altaisoft@gmail.com>
2026-03-08 19:43:18 +0400

    Remove deleted tests, update spec URL to w3c.github.io, use jsonld.JsonLdError

--- tests/test_expand.py
+++ tests/test_expand.py
@@ -1,12 +1,10 @@
 import json
 
 import pytest
-from dirty_equals import IsList, IsPartialDict
+from dirty_equals import IsPartialDict
 from pyld import jsonld
-from pyld.jsonld import JsonLdError
 
 import yaml_ld
-from tests.common import specifications_root
 
 
 def test_pyld_expand_json_string():
@@ -20,7 +18,7 @@ def test_pyld_expand_json_string():
         'something': None,
     }
 
-    with pytest.raises(JsonLdError) as error_info:
+    with pytest.raises(jsonld.JsonLdError) as error_info:
         jsonld.expand(json.dumps(document))
 
     assert error_info.value.type == 'jsonld.InvalidUrl'
@@ -40,24 +38,15 @@ def test_empty_value():
     assert not yaml_ld.expand(document)
 
 
-def test_local_context():
-    document = specifications_root / 'json-ld-api/tests/expand/0127-in.jsonld'
-    yaml_ld.expand(document)
-
-
-def test_api_html_e016():
-    source = specifications_root / 'json-ld-api/tests/html/e016-in.html'
-    jsonld.expand(source)
-
 
 def test_https():
     response = yaml_ld.expand(
-        'https://json-ld.github.io/yaml-ld/tests/manifest.jsonld',
+        'https://w3c.github.io/yaml-ld/tests/manifest.jsonld',
     )
 
     assert response == [
         IsPartialDict({
-            '@id': 'https://json-ld.github.io/yaml-ld/tests/basic-manifest',
+            '@id': 'https://w3c.github.io/yaml-ld/tests/basic-manifest',
         }),
     ]
 
